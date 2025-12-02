3665abbb28d497d154fb7869a39ab023245863f1
jmellibosky <joaquinm.utn@gmail.com>
2025-12-02 14:04:58 -0300

    merge

---
+++ backend/HotelAPI/Enums/ResponseSender.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelAPI.Enums
+{
+    public static class ResponseSender
+    {
+        public static ActionResult<T> Send200<T>(this ControllerBase controller, T response)
+        {
+            return controller.Ok(response);
+        }
+
+        public static ActionResult Send400(this ControllerBase controller, string response)
+        {
+            return controller.BadRequest(response);
+        }
+
+        public static ActionResult Send404(this ControllerBase controller, string response)
+        {
+            return controller.NotFound(response);
+        }
+
+        public static ActionResult Send500(this ControllerBase controller, string response)
+        {
+            return controller.StatusCode(StatusCodes.Status500InternalServerError, response);
+        }
+    }
+}
---
+++ backend/HotelAPI/Enums/ResponseStrings.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelAPI.Enums
+{
+    public static class ResponseStrings
+    {
+        public const string AMOUNT_OVER_ZERO = "Amount must be over zero.";
+        public const string DRINK_NOT_FOUND = "Drink was not found in the database.";
+        public const string GAME_NOT_FOUND = "Game was not found in the database.";
+        public const string GAME_OR_DRINK_EMPTY = "Either Game or Drink parameters must be provided.";
+        public const string PASS_EMPTY = "Pass parameter was empty.";
+        public const string ROOM_EMPTY = "Room parameter was empty.";
+        public const string ROOM_NOT_FOUND = "Room was not found in the database.";
+        public const string ROOM_NO_LAST_MOVEMENT = "Room has no last movement.";
+        public const string USER_EMPTY = "User parameter was empty.";
+        public const string USER_NOT_FOUND = "User was not found in the database.";
+    }
+}
